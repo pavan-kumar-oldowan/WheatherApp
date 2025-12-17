3a83b7cf46d5e342bc2e3d70bd9b806fec692804
PavanKumar <pavankumar.chittiboina@oldowaninnovations.com>
2025-12-17 14:54:37 +0530

    feat:add City Search with TypeScript

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,39 +1,61 @@
-import  {View,Text,StyleSheet} from "react-native";
-import React ,{useEffect,useState} from "react"
+import  {View,Text,StyleSheet, TextInput,TouchableOpacity} from "react-native";
+import React ,{useState,useEffect}from "react"
 import {fetchWeatherByCity} from "../services/weatherApi";
 import { WeatherResponse } from "@/types/wheather";
+
 const HomeScreen:React.FC= ()=>{
     const [weather,setWeather] = useState<WeatherResponse|null>(null)
-    const [Loading,setLoading] = useState(false);
+    const [Loading,setLoading] = useState<boolean>(false);
+    const [city,setCity] = useState<string>("London");
+    const [error,setError] = useState<string>("");
+
+    const getWeather = async()=>{
+         if(!city.trim()){
+            setError("Please Enter a City Name");
+            return;
+         }
+         setLoading(true);
+         setError("");
+
+      try{
+        const data = await fetchWeatherByCity(city);
+        setWeather(data);
+        console.log("WeatherData:",data);
+         }
+       catch(e){
+            console.log(e)
+         }
+       finally{
+            setLoading(false)
+         }
+    }
 
     useEffect(()=>{
-        const loadWeather = async()=>{
-            try{
-                const data = await fetchWeatherByCity("kadapa");
-                setWeather(data);
-                console.log("WeatherData:",data)
-            }catch(e){
-                console.log(e);
-            }
-            finally{
-                setLoading(false);
-            }
-        } 
-        loadWeather(); 
-
+         getWeather()
     },[])
-    if(Loading){
-        return(
-            <View style={styles.container}>
-            <Text>Loading...</Text>
-            </View>
-        )
-    }
+
     return (
         <View style={styles.container}>
-          <Text style={styles.text}>{weather?.name}</Text>
-          <Text>Temperature: {weather?.main.temp}°C</Text>
-          <Text>Condition: {weather?.weather[0].description}</Text>
+          <Text style={styles.text}>Weather App</Text>
+          <TextInput style={styles.input} 
+                    placeholder="enter a city" 
+                    value={city} 
+                    onChangeText={(text:string)=>setCity(text)}
+            />
+           <TouchableOpacity style={styles.button} onPress={getWeather}>
+               <Text style={styles.buttontext}>Search</Text>
+           </TouchableOpacity>
+           {Loading && <Text>Loading ....</Text>}
+           {error ?<Text style={styles.error}>{error}</Text>:null}
+           {weather && !Loading && (
+              <View style={styles.result}>
+                 <Text style={styles.city}>{weather.name}</Text>
+                 <Text>Temperature:{weather.main.temp}</Text>
+                 <Text>Condition:{weather.weather[0].main}</Text>
+                 <Text>Humidity:{weather.main.humidity}%</Text>
+              </View>
+           )}
+
         </View>
     )
 }
@@ -45,6 +67,39 @@ const styles = StyleSheet.create({
        },text:{
         fontSize:26,
         fontWeight:"bold"
+       },
+       input:{
+         borderWidth:1,
+         borderColor:"#ccc",
+         borderRadius:8,
+         padding:10,
+         marginBottom:10
+
+       },
+       button:{
+          backgroundColor:'#007AFF',
+          borderRadius:8,
+          padding:10,
+          alignItems:"center"
+       },
+       buttontext:{
+           color: '#fff',
+           fontWeight: 'bold',
+       },
+       error:{
+           color:"red",
+           textAlign:"center",
+           marginTop:10
+       },
+       result:{
+        marginTop:20,
+        alignItems:"center"
+       },
+       city:{
+        fontSize:20,
+        fontWeight:"bold"
        }
+
+
 })
 export default HomeScreen
